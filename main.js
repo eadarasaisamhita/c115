7826f29e747296e1a1e2490ca27ba699715ccc85
eadarasaisamhita <72115037+eadarasaisamhita@users.noreply.github.com>
2020-11-05 20:50:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
-  
+  clown_nose=loadImage('https://i.postimg.cc/pLQDRSz5/clown-nose.png');
 }
 
 function setup() 
@@ -7,10 +10,10 @@ function setup()
   canvas = createCanvas(300, 300);
   canvas.center();
   video = createCapture(VIDEO);
-  vidoe.size(300, 300);
+  video.size(300, 300);
   video.hide();
 
-  postNet= ml5.postNet(video, modelLoaded);
+  poseNet= ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
 
@@ -23,14 +26,20 @@ function gotPoses(results)
 {
   if(results.length>0){
     console.log(results);
-    console.log("nose x="+results[0].pose.nose.x);
-    console.log("nose y="+results[0].pose.nose.y);
+    noseX=results[0].pose.nose.x-15;
+    noseY=results[0].pose.nose.y-15;
+    console.log("nose x="+noseX);
+    console.log("nose y="+noseY);
   }
 }
 
 function draw() 
 {
   image(video,0 ,0 ,300 ,300);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(noseX, noseY, 20);
+  image(clown_nose,noseX,noseY,30,30)
 }
 
 function take_snapshot() 
